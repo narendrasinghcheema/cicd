8a1808c166aa75e727f57119b31566a3c43b7c84
narendrasinghcheema <narendrasinghcheema348@gmail.com>
2025-04-24 09:18:40 +0000

    added

--- app.py
+++ app.py
@@ -20,9 +20,11 @@ cursor.execute("""
         first_name VARCHAR(100),
         middle_name VARCHAR(100),
         last_name VARCHAR(100),
-        father_name VARCHAR(100),
-        mother_name VARCHAR(100),
-        dob DATE
+        dob DATE,
+        email VARCHAR(150),
+        phone VARCHAR(20),
+        address VARCHAR(255),
+        occupation VARCHAR(100)
     )
 """)
 db.commit()
@@ -33,18 +35,20 @@ def index():
 
 @app.route('/submit', methods=['POST'])
 def submit():
-    first_name = request.form['fname']
-    middle_name = request.form['mname']
-    last_name = request.form['lname']
-    father_name = request.form['fathername']
-    mother_name = request.form['mothername']
+    first_name = request.form['first_name']
+    middle_name = request.form['middle_name']
+    last_name = request.form['last_name']
     dob = request.form['dob']
+    email = request.form['email']
+    phone = request.form['phone']
+    address = request.form['address']
+    occupation = request.form['occupation']
 
     sql = """
-        INSERT INTO users (first_name, middle_name, last_name, father_name, mother_name, dob)
-        VALUES (%s, %s, %s, %s, %s, %s)
+        INSERT INTO users (first_name, middle_name, last_name, dob, email, phone, address, occupation)
+        VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
     """
-    values = (first_name, middle_name, last_name, father_name, mother_name, dob)
+    values = (first_name, middle_name, last_name, dob, email, phone, address, occupation)
     cursor.execute(sql, values)
     db.commit()
 
